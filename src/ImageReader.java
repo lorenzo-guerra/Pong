e5a0e2e3b46eb7dfdace71113ab72b2710b854d5
aroundlylily <dylan.yurillo@gmail.com>
2023-01-14 18:03:14 -0600

    pushing ball physics

--- src/ImageReader.java
+++ src/ImageReader.java
@@ -3,19 +3,28 @@ import javax.swing.*;
 import java.io.*;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
+import java.awt.event.*;
+import java.util.HashMap;
 
 public class ImageReader {
+    private HashMap<String, JLabel> imageCache = new HashMap();
     
     ImageReader() {}
     
-    public JLabel addAsset(String assetName, int xPosition, int yPosition) {
+    public JLabel addAsset(String assetName, String filePath, int xPosition, int yPosition) {
         BufferedImage imageBuffer = null;
-        try {
-            imageBuffer = ImageIO.read(new File("assets/" + assetName));
+        if (imageCache.get(assetName) == null) {
+            try {
+                imageBuffer = ImageIO.read(new File(filePath));
+            }
+            catch(IOException printException) {}
+            JLabel newAsset = new JLabel(new ImageIcon(imageBuffer));
+            newAsset.setBounds(xPosition, yPosition, imageBuffer.getWidth(), imageBuffer.getHeight());
+            imageCache.put(assetName, newAsset);
+            return newAsset;
         }
-        catch(IOException printException) {}
-        JLabel newAsset = new JLabel(new ImageIcon(imageBuffer));
-        newAsset.setBounds(xPosition, yPosition, imageBuffer.getWidth(), imageBuffer.getHeight());
-        return newAsset;
+        JLabel asset = imageCache.get(assetName);
+        asset.setBounds(xPosition, yPosition, asset.getWidth(), asset.getHeight());
+        return asset;
     }
 }
--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,21 +1,45 @@
 import java.awt.*;
 import javax.swing.*;
 import java.io.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.awt.event.*;
 
 public class MainWindow {
     JFrame windowObject = new JFrame("Pong");
     ImageReader imageConstructor = new ImageReader();
-    JLabel background = imageConstructor.addAsset("background.png", 0, 0);
-    JLabel ball = imageConstructor.addAsset("ball.png", 500, 300);
-    JLabel leftPaddle = imageConstructor.addAsset("paddle.png", 20, 210);
-    JLabel rightPaddle = imageConstructor.addAsset("paddle.png", 970, 350);
+    JLabel background = imageConstructor.addAsset("background", "assets/background.png", 0, 0);
+    int xVelocity = 10, yVelocity = 10;
+    int xPosition = 500, yPosition = 300;
+    JLabel ball = imageConstructor.addAsset("ball", "assets/ball.png", yPosition, xPosition);
+    JLabel leftPaddle = imageConstructor.addAsset("leftPaddle", "assets/paddle.png", 20, 210);
+    JLabel rightPaddle = imageConstructor.addAsset("rightPaddle", "assets/paddle.png", 970, 350);
+    javax.swing.Timer updatePosition;
+    
+    ActionListener incrementPosition = new ActionListener () {
+        public void actionPerformed(ActionEvent e) {
+            xPosition += xVelocity;
+            yPosition += yVelocity;
+            if (xPosition + 10 == 1000 || xPosition == 0) {
+                xVelocity *= -1;
+            }
+            if (yPosition + 10 == 600 || yPosition == 0 ) {
+                yVelocity *= -1;
+            }
+            background.add(imageConstructor.addAsset("ball", "assets/ball.png", xPosition, yPosition));
+        }
+    };
     
     MainWindow() {
         ConstructWindow();
+        updatePosition = new javax.swing.Timer(1, incrementPosition);
+        updatePosition.start();
+
     }
     
     private void ConstructWindow() {
         windowObject.add(background);
+        windowObject.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         background.add(ball);
         background.add(leftPaddle);
         background.add(rightPaddle);
